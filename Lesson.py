c463c210449c030bc090c229948df7d8f07760b0
mschen-sta <cherrie.chen@ycdsb.ca>
2024-03-14 09:33:43 -0400

    Added lesson examples

--- Lesson.py
+++ Lesson.py
@@ -1,1 +1,24 @@
+#-------------------------------------------------------------------------
+# Name:		    Lesson 3.3 User Input & Casting
+# Purpose:		Lesson Examples
+# Author:		Chen. C
+#
+# Created:		13/03/2024
+#-------------------------------------------------------------------------
 
+'''
+User Input
+'''
+# Example #1: User inputs their first name
+
+
+# Example #2: Checking the type of first_name
+
+
+# Example #3: User inputs their age. What is the data type?
+
+
+'''
+Casting
+'''
+# Example #4: Change the user input from Example #3 to an integer.
