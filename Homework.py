f807ace41439d434af93cd6ccca8c84b2f315f4e
mschen-sta <cherrie.chen@ycdsb.ca>
2024-03-14 09:33:54 -0400

    Created homework Qs

--- Homework.py
+++ Homework.py
@@ -1,1 +1,36 @@
+#-------------------------------------------------------------------------
+# Name:		    Lesson 3.3 User Input & Casting
+# Purpose:		Homework Questions
+# Author:		Last Name. First Initial
+#
+# Created:		dd/mm/yyyy
+#-------------------------------------------------------------------------
 
+# QUESTION #1:
+# Write a program takes the user's first name, last name, and age.
+# The program will then output their information
+
+
+#The output should look like this:
+'''
+What is your first name? Jane
+What is your last name? Doe
+What is your age? 23
+
+First Name: Jane
+Last Name: Doe
+Age: 23
+'''
+
+# QUESTION #2:
+# Write a program that asks the user for each data type (int, float, string).
+# Make sure to use variables and follow the Python Style Guide
+
+
+# Part (a) Convert each type to an int then output the type of each one
+
+
+# Part (b) Convert each type to a float then output the type of each one
+
+
+# Part (c) Convert each type to a string then output the type of each one
